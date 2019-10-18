2fbd189e40c4299ce92f066f6a7999a7fc67e6f2
magrcruz <50184830+magrcruz@users.noreply.github.com>
2019-10-17 23:34:43 -0500

    Update inventario.cpp

--- src/inventario.cpp
+++ src/inventario.cpp
@@ -1,11 +1,19 @@
-#include "inventario.h"
+#include "objetoInventario.h"
 
-inventario::inventario()
+objetoInventario::objetoInventario()
 {
     //ctor
 }
 
-inventario::~inventario()
+objetoInventario::~objetoInventario()
 {
     //dtor
+}
+
+void setEstanteSillaMesa(int s,int m,int e){
+    sillas.clear();
+    mesas.clear();
+    sillas.clear();
+    for (int i=0;i<(s+m+e);i++)
+        sillas.push_back(objetoInventario);
 }
