92c51fe35da978b786b0542194b2241311ae68c5
Solcito25 <51237026+Solcito25@users.noreply.github.com>
2019-10-20 20:27:54 -0500

    notas

--- include/notas.h
+++ include/notas.h
@@ -1,18 +1,20 @@
 #ifndef NOTAS_H
 #define NOTAS_H
 #include <string>
-
+using namespace std;
 class notas
 {
     public:
         notas();
         virtual ~notas();
-
+        void NotaporItem(char nombreItem,char nota, string comentario);
+        bool CantidadCaracteres(string comentario);
     protected:
 
     private:
         char nombreItem[200];
-        char nota, comentario[350];
+        char nota;
+        string comentario;
 };
 
 #endif // NOTAS_H
