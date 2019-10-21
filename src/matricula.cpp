7d4b1678accccdf0bbf69547d37fc714e4060019
Solcito25 <51237026+Solcito25@users.noreply.github.com>
2019-10-20 21:12:45 -0500

    matricula

--- src/matricula.cpp
+++ src/matricula.cpp
@@ -2,10 +2,22 @@
 
 matricula::matricula()
 {
-    //ctor
+    matricula :: pago=0;
+    matricula :: deuda=150;
+    
 }
 
 matricula::~matricula()
 {
     //dtor
+}
+
+void matricula :: getDeuda(int pago, int deuda){
+ if (pago<150){
+    deuda=150-pago;
+    cout<<"Su deuda es de "<<deuda<<endl;
+}
+ else 
+   deuda=0;
+   cout<<"No tiene deuda"<<endl;
 }
