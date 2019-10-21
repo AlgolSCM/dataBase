fd5ff0fb81c2b04797eb694c264ef0dfacaa5ab5
magrcruz <50184830+magrcruz@users.noreply.github.com>
2019-10-20 22:42:45 -0500

    Update Alumno.cpp

--- src/Alumno.cpp
+++ src/Alumno.cpp
@@ -5,6 +5,11 @@ Alumno::Alumno()
     //ctor
 }
 
+Alumno :: Alumno(string apellidos,string nombres){
+    Alumno::apellido=apellidos;
+    Alumno::nombre=nombres;
+}
+
 Alumno::~Alumno()
 {
     //dtor
