e77c9b29384f501de9b3c77550993ae23582ebad
Solcito25 <51237026+Solcito25@users.noreply.github.com>
2019-10-20 20:57:40 -0500

    notas

--- src/notas.cpp
+++ src/notas.cpp
@@ -9,3 +9,27 @@ notas::~notas()
 {
     //dtor
 }
+bool notas :: CantidadCaracteres(string comentario){
+unsigned int tam=comentario.length();
+if( tam>=10 && tam<=350)
+    return true;
+else
+    return false;
+}
+bool notas :: CantidadCaracteres(string comentario){
+unsigned int tam=comentario.length();
+if( tam>=10 && tam<=350)
+    return true;
+else
+    return false;
+}
+
+void notas :: NotaporItem(string nombreItem,char nota, string comentario){
+if (CantidadCaracteres(comentario)== true){
+    cout<< nombreItem<<endl;
+    cout<<"Nota"<<setw(10)<<"Conclusion Descriptiva"<<endl;
+    cout<<nota<<setw(10)<<comentario<<endl;
+}
+else
+    cout<<"No cumple con la cantidad de caracteres solicitada, vuelva a ingresar"<<endl;
+}
