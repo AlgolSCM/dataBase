909dbf431edcdcfe371256e718a3481d47900600
camilaalj <camilaalj6@gmail.com>
2019-10-22 18:35:55 -0500

    cambios

--- include/Alumno.h
+++ include/Alumno.h
@@ -50,7 +50,28 @@ class Alumno
         talleres listaTalleres[5];
         Padres nombrePadres[2];
         vector <notas> calificaciones(0);
-        
+        matricula::matricula()
+        {
+            matricula :: pago=0;
+            matricula :: deuda=150;
+
+        }
+
+        matricula::~matricula()
+        {
+            //dtor
+        }
+
+        void matricula :: getDeuda(int pago, int deuda){
+        if (pago<150){
+            deuda=150-pago;
+            cout<<"Su deuda es de "<<deuda<<endl;
+        }
+        else
+        deuda=0;
+        cout<<"No tiene deuda"<<endl;
+        }
+
 
 };
 
