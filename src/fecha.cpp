9ecea375f255431387312da740664fc23cb04878
magrcruz <50184830+magrcruz@users.noreply.github.com>
2019-10-22 22:58:42 -0500

    Correcta implementacion de Fecha y funciones

--- src/fecha.cpp
+++ src/fecha.cpp
@@ -1,53 +1,74 @@
 #include "fecha.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 fecha::fecha(){
-    cout<< "Fecha creada por defecto 25/08/11"<<endl;
-    fecha::dia=25;
-    fecha::mes=8;
-    fecha::anio=11;
+    fecha::dia=1;
+    fecha::mes=1;
+    fecha::anio=1;
 }
-fecha::~fecha(){
-    //dtor
-}
-
-
-bool fecha :: setDiaMesAnio(int a,int b,int c){
+fecha::fecha(int a,int b, int c){
+    fecha::setDiaMesAnio(a,b,c);
+    }
+void fecha :: setDiaMesAnio(int a,int b,int c){
     if (validarFecha(a,b,c)){
         fecha::dia=a;
         fecha::mes=b;
         fecha::anio=c;
-        return 1;
+        cout<<"Fecha modificada al: ";
+        fecha::coutDMMAA(1);
+        return;
     }
-    return 0;//retorna si se modifico la fecha
+    cout<<"Fecha no valida"<<endl;
+    setDiaMesAnio(1,1,1);
 }
-
-
 bool fecha :: validarFecha(int a,int b,int c){//verifica si la fecha es real, no verifica el anio
-    if (fecha::mes>=1&&fecha::mes<=12){//ve si el mes es valido
-        if (fecha::mes==2){//verifica el valor del dia febrero se evalua tambien si el anio fue bisiesto
-            if (fecha::dia>=1&&fecha::dia>=28)
+    if (b>=1 && b<=12){//ve si el mes es valido
+        if (b==2){//verifica el valor del dia febrero se evalua tambien si el anio fue bisiesto
+            if (a>=1 && a<=28)
                 return 1;
-            else if (fecha::dia==29&&!(fecha::anio%4))//si el anio fue bisiesto califica el 29
+            else if (a==29 && !(c%4))//si el anio fue bisiesto califica el 29
                 return 1;
+            return 0;
         }
-        else if (fecha::dia<=30&&fecha::dia>=1)//evalua si el dia es valido
+        else if (a<=30 && a>=1)//evalua si el dia es valido
             return 1;
-        else if (fecha::dia==31){//evalua si tiene 31 dias
-            if (fecha::mes<=7&&(fecha::mes%2))//evalua los meses hasta julio, los impares tienen 31 dias
+        else if (a==31){//evalua si tiene 31 dias
+            if (b<=7 && (b%2))//evalua los meses hasta julio, los impares tienen 31 dias
                 return 1;
-            else if(!(fecha::mes%2))//si es un mes par y tiene 31 dias es valido
+            else if( !(b%2) )//si es un mes par y tiene 31 dias es valido
                 return 1;
         }
     }
     return 0;
 }
-
-void coutDMA(bool verAnio=1){
-    cout<<fecha::dia<<"/"<<fecha::mes;
-    if (verAnio)
+void fecha::coutDMA(bool verAnio){
+    if (fecha::dia<10) cout<<0;
+    cout<<fecha::dia<<"/";
+    if (fecha::mes<10) cout<<0;
+    cout<<fecha::mes;
+    if (verAnio){
+        if (fecha::anio<10) cout<<0;
         cout <<"/"<<fecha::anio;
+    }
     cout<<endl;
+}
+void fecha::coutDMMAA(bool verAnio){
+    string meses[]={"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"};
+    cout<<fecha::dia<<" de "<<meses[(fecha::mes-1)];
+    if (verAnio){
+        cout <<" del 20";
+        if (fecha::anio<10) cout<<0;
+        cout<<fecha::anio;
+        }
+    cout<<endl;
+}
+bool fecha::paso2a3(){//calcula si nacio antes de esta fecha
+    int a=31,b=3,c=2019-3;
+    if (fecha::anio<c) return 1;//nacio antes de 2019-3=2016 tiene mas de 3 anios al menos
+    else if (fecha::anio==c)
+        if (fecha::mes<=b) return 1;
+    return 0;
 }
