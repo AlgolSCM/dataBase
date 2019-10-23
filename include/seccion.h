19f461150eb19fa7f982d27ae3d3fa18b118fc11
magrcruz <50184830+magrcruz@users.noreply.github.com>
2019-10-22 23:22:15 -0500

    Anadir, modificar y eliminar funciones de seccion
    las secciones seran instancias en main con los nombres  exploradoras2,creativas3,constructivas4,reflexivas5.
    Esta clase trata al conjunto de alumnos de determinado nivel pudiendo clasificarlos mejor

--- include/seccion.h
+++ include/seccion.h
@@ -7,16 +7,38 @@ class seccion
     public:
         seccion();
         virtual ~seccion();
-        void asignarSeccion(fecha fechaNacimiento);
-        void llenarExploradoras(char *exploradoras2);
-        void llenaCreativas(char *creativas3);
-        void llenarConstructivas(char *constructivas4);
-        void llenarReflexivas(char *reflexivas5);
         void getNinosNinas();
+
+        void abrirMenuSeleccion();//mostrara el menu de las opciones
+
+        void seleccionarAlumno(int n);//abre el menu de opciones alumno segun ubicacion en la lista
+        void verListadoAlumnos();
+        void addAlumno();
+        void deleteAlumno(int n);//solicita el numero de lista del alumno
+
+        void verAsistencias();
+        void tomarAsistencias();
+
+        void verNotas();
+        void addNotas();
+        void modificarNotas(int n);//numero n del vector de notas
+
+        void verCursosTalleres();
+        void asignarCursosTalleres();
+        
+        void verAnecdotario();
+        void addAnecdotario();
+        void deleteAnecdotario(int n);
+        
+        void verInventario();
+        void addInventario();
+        void modificarEstadoInventario(int n);//posicion n del vector inventario
+        void deleteInventario();
+        
+                
     protected:
 
     private:
-        char exploradoras2[20],creativas3[20],constructivas4[20],reflexivas5[20];
 
 
 };
