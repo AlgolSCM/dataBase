23208baa85c9d3be08af4a7a89a7c526de271798
magrcruz <50184830+magrcruz@users.noreply.github.com>
2019-10-23 09:53:14 -0500

    Update seccion.cpp

--- src/seccion.cpp
+++ src/seccion.cpp
@@ -1,5 +1,9 @@
 #include "seccion.h"
+#include "Alumno.h"
+#include <iostream>
+#include <string>
 
+using namespace std;
 seccion::seccion()
 {
     //ctor
@@ -8,4 +12,41 @@ seccion::seccion()
 seccion::~seccion()
 {
     //dtor
+}
+
+int seccion::ingreseSeleccion(){
+    int n;
+    cout<<"Ingrese su seleccion: ";
+    cin>>n;
+    cout<<endl;
+    return n;
+}
+void imprimirOpcionesSeleccion(bool ver,bool anadir,bool eliminar,bool modificar){
+    string opcion[]=
+    {"ver",
+    "anadir",
+    "eliminar",
+    "modificar",
+   };
+}
+
+void seccion::menuSeleccion(){
+    string opcion[]=
+    {"ALUMNO (Seleccionar, anadir, eliminar)",
+    "ASISTENCIAS (Ver y anadir)",
+    "NOTAS",//Sol
+    "CURSOS",//Sol
+    "ANECDOTARIO",//Camila
+    "INVENTARIO"//Camila
+    };
+    for (int i=0;i<6;i++)
+        cout<<i+1<<". "<<opcion[i]<<endl;
+    switch(ingreseSeleccion()){
+        case 1:
+            //Alumno::menuSeleccion();
+            break;
+        default:
+            cout<<"Comida"<<endl;
+            break;
+    }
 }
