e520884719e170ad5d8f2dd4de8c3c45c7521b81
Solcito25 <51237026+Solcito25@users.noreply.github.com>
2019-10-23 09:48:54 -0500

    exp medico

--- src/expedienteMedico.cpp
+++ src/expedienteMedico.cpp
@@ -5,8 +5,8 @@ using namespace std;
 expedienteMedico::expedienteMedico()
 {
     sento=0;paro=0;camino=0;palabra=0;hablo=0;gateo=0;esfinteres=0;fluidez=0;
-    controlaEsfinteres="SI";tipoSangre="O+";
-    alergias="";vacunas="";enfermedades="";traumas="";
+    controlaEsfinteres=true;partonatural=true;tipoSangre="O+";
+    alergias="";vacunas="";enfermedades="";traumas="";partodescripcion="";
 
 }
 
@@ -15,35 +15,54 @@ expedienteMedico::~expedienteMedico()
     //dtor
 }
 
-void expedienteMedico :: setDesarrollo(int sento1, int paro1, int camino1, int palabra1,int hablo1,int gateo1,int fluidez1){
-    sento=sento1;
-    paro=paro1;
-    camino=camino1;
-    hablo=hablo1;
-    gateo=gateo1;
-    fluidez=fluidez1;
+void expedienteMedico :: setDesarrollo(int sento, int paro, int camino, int palabra,int hablo,int gateo,int fluidez, bool controlaEsfinteres,bool partonatural,string tipoSangre){
+    this -> sento=sento;
+    this -> paro=paro;
+    this -> camino=camino;
+    this -> hablo=hablo;
+    this -> gateo=gateo;
+    this -> fluidez=fluidez;
+    this -> controlaEsfinteres=controlaEsfinteres;
+    this -> partonatural=partonatural;
+    this -> tipoSangre=tipoSangre;
 }
-void expedienteMedico::ControlEsfinteres(string controlaEsfinteres1,int esfinteres=0){
-  if (controlaEsfinteres1=="SI"){
+void expedienteMedico::ControlEsfinteres(bool controlaEsfinteres,int esfinteres=0){
+  if (controlaEsfinteres==true){
       cout<<"Ingrese la edad"<<endl;
       cin>>esfinteres;
   }
 }
-void expedienteMedico::setDatosMedicos(string alergias1,string vacunas1,string enfermedades1,string trauma1){
-    alergias=alergias1;
-    vacunas=vacunas1;
-    enfermedades=enfermedades1;
-    traumas=trauma1;
+void expedienteMedico::setDatosMedicos(string alergias,string vacunas,string enfermedades,string trauma){
+    this -> alergias=alergias;
+    this -> vacunas=vacunas;
+    this -> enfermedades=enfermedades;
+    this -> traumas=traumas;
 }
 
-void expedienteMedico::TipodeSangre(string tipoSangre1){
-     tipoSangre=tipoSangre1;
+void expedienteMedico::Parto(bool parto,string partodescripcion){
+    if (parto==false){
+      cout<<"Ingrese la complicación"<<endl;
+      cin>>partodescripcion;
+    }
 }
-void expedienteMedico::ExpedienteMedico(){
+void expedienteMedico::Mostrar(){
+
     cout<<"Se sento: "<<sento<<"meses"<<endl;
     cout<<"Se paro: "<<paro<<"meses"<<endl;
     cout<<"Camino: "<<camino<<"meses"<<endl;
-    cout<<"Controlo Esfinteres: "<<controlaEsfinteres<<"edad: "<<esfinteres<<endl;
+    if (controlaEsfinteres==true){
+        cout<<"Controla Esfinteres: Si "<<"Edad: "<<esfinteres<<endl;
+        }
+    else{
+        cout<<"Controla Esfinteres: No "<<"Edad: --- "<<endl;
+        }
+    if (partonatural==true){
+        cout<<"Parto: Natural "<<"Complicacion:--- "<<endl;
+        }
+    else{
+        cout<<"Parto: Complicado"<<"Complicacion: "<<partodescripcion<<endl;
+        }
+
     cout<<"Dijo su primera palabra: "<<palabra<<endl;
     cout<<"Hablo con fluidez: "<<fluidez<<endl;
     cout<<"Gateo: "<<gateo<<endl;
