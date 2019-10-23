b281f518b2093ab84e2a716dec6024ae424d44a8
Solcito25 <51237026+Solcito25@users.noreply.github.com>
2019-10-23 09:47:59 -0500

    exp medico

--- include/expedienteMedico.h
+++ include/expedienteMedico.h
@@ -7,12 +7,13 @@ class expedienteMedico
     public:
         expedienteMedico();
         virtual ~expedienteMedico();
-        void setDesarrollo(int sento1, int paro1, int camino1, int palabra1,int hablo1,int gateo1,int fluidez);
-        void ControlEsfinteres(string controlaEsfinteres1,int esfinteres1);
+        void setDesarrollo(int sento1, int paro1, int camino1, int palabra1,int hablo1,int gateo1,int fluidez, bool controlaEsfinteres,bool parto,string tipoSangre);
+        void ControlEsfinteres(bool controlaEsfinteres1,int esfinteres1);
+        void Parto(bool partonatural, string partodescripcion);
         void setDatosMedicos(string alergias1, string vacunas1, string enfermedades1, string trauma1);
-        void TipodeSangre(string tipoSangre1);
 
-        void ExpedienteMedico();
+
+        void Mostrar();
 
 
     protected:
@@ -20,8 +21,8 @@ class expedienteMedico
     private:
         int sento, paro, camino, palabra, hablo, gateo, esfinteres,fluidez;
         string tipoSangre;
-        string controlaEsfinteres;
-        string alergias, vacunas, enfermedades, traumas;
+        bool controlaEsfinteres,partonatural;
+        string alergias, vacunas, enfermedades, traumas,partodescripcion;
 };
 
 #endif // EXPEDIENTEMEDICO_H
