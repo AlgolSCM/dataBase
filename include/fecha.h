08ab23e5175d3577392a4063815192733c515bb7
magrcruz <50184830+magrcruz@users.noreply.github.com>
2019-10-22 22:57:44 -0500

    Correcta implementacion de Fecha y funciones

--- include/fecha.h
+++ include/fecha.h
@@ -1,36 +1,21 @@
 #ifndef FECHA_H
 #define FECHA_H
-#include <string>
 
 class fecha
 {
     public:
         fecha();
-        fecha(int a,int b,int c);
-        virtual ~fecha();
-        int dia, mes, anio;
-        /*
-        Chicass, duda existencial, fecha no es solo un valor de dias meses y anios que podemos usar para calcular si pasa o no
-        osea... no le veo sentido a estas funciones la verdad, cuando nos veamos les explico
-        void FechaAlumno(int dia, int mes, int anio);
-        void FechaMatricula(int dia, int mes, int anio);
-        void FechaInventario(int dia, int mes, int anio);
-        void FechaPadre(int dia, int mes, int anio);
-        */
-
-        bool setDiaMesAnio(int a=1,int b=1,int c=1);
-        bool validarFecha(int a,int b,int c);
-        void getTiempoDias(int a,int b,int c);//Ingresar fecha actual
-        //cuantos dias transcurrieron desde la fecha sin contar hoy
-        void coutDMA(bool verAnio=1);// 12/12/12
-        void coutDMMAA(bool verAnio=1);// 12 setiembre del 2012
-        bool coutPasa();//ver si el pequeno pasa a 3 anitos
+        fecha(int a,int b,int c);//constructor
+        void setDiaMesAnio(int a,int b,int c);//cambiar la fecha
+        bool validarFecha(int a,int b,int c);//Ve si la fecha existe
+        void coutDMA(bool verAnio);//En formato 12/12/12
+        void coutDMMAA(bool verAnio);//En formato 12 setiembre del 2012
+        bool paso2a3();//ver si el pequeno pasa de 2 a 3 anitos
 
     protected:
 
     private:
-
-
+        int dia, mes, anio;
 };
 
 #endif // FECHA_H
