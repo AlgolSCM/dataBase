d7cf4ca52ee36800b18ce85bb8e60175d67f1271
Solcito25 <51237026+Solcito25@users.noreply.github.com>
2019-10-22 19:38:51 -0500

    exp medico

--- include/expedienteMedico.h
+++ include/expedienteMedico.h
@@ -8,13 +8,23 @@ class expedienteMedico
     public:
         expedienteMedico();
         virtual ~expedienteMedico();
+        void setDesarrollo(int sento, int paro, int camino, int palabra,int hablo,int gateo);
+        void Esfinteres(char controlaEsfinteres);
+        void setDatosMedicos(string alergias, string vacunas, string enfermedades, string trauma);
+        void TipodeSangre(char tipoSangre);
+
+        void getDesarrollo();
+        void getDatosMedicos();
+        void getTipodeSangre();
+        void getEsfinteres();
+
 
     protected:
 
     private:
         int sento, paro, camino, palabra, hablo, gateo, esfinteres;
         char tipoSangre[4];
-        bool controlaEsfinteres;
+        char controlaEsfinteres[3];
         string alergias, vacunas, enfermedades, traumas;
 };
 
