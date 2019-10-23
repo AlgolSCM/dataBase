b2720ebc5420477d38edea533f678f3a256405d6
camilaalj <54364115+camilaalj@users.noreply.github.com>
2019-10-23 09:58:04 -0500

    Update seccion.h

--- include/seccion.h
+++ include/seccion.h
@@ -34,11 +34,27 @@ class seccion
         void addInventario();
         void modificarEstadoInventario(int n);//posicion n del vector inventario
         void deleteInventario();
+        /**inventario();
+        virtual ~inventario();
+        void setEstanteSillaMesa(int sillas,int mesas,int estantes);
+        void imprimirInventario();
+        void imprimirArrayInventario(string nombre);
+        void modificarInventario();
+        //Imprime una lista numerada de sillas a manera de tabla y permite modificarla, las primeras opciones de varias a la vez
+        //cambiarNumero, cambiarEstado
+        //El resto permita selecionar una sola y permita cambiar modelo, estado y demas caracteristicas*/
 
 
     protected:
 
     private:
+        /**vector <objetoInventario> sillas;//son arrays dinamicos de diferentes objetos
+        vector <objetoInventario> estantes;
+        vector <objetoInventario> mesas;
+
+        void cambiarNumeroObjeto(int s,int m,int e);//puede ser negativo o positivo
+        void cambiarEstadoObjeto(int s,int m,int e,bool anadirS,bool anadirM,bool anadirE);
+        //cambia n objetos en el estado del bool*/
 
 
 };
