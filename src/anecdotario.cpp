d6683378bb8645b68f7ff24dead2cf1056c2cd17
camilaalj <54364115+camilaalj@users.noreply.github.com>
2019-10-25 09:31:20 -0500

    Update anecdotario.cpp

--- src/anecdotario.cpp
+++ src/anecdotario.cpp
@@ -1,10 +1,21 @@
 #include "anecdotario.h"
+#include <iostream>
+#include "fecha.h"
+using namespace std;
+
 
 anecdotario::anecdotario()
 {
     //ctor
 }
 
+anecdotario::anecdotario(int implicadosnumero,string suceso, string descripcionde, fecha acontecimiento)
+{
+    titulo = suceso;
+    descripcion = descripcionde;
+    fecha dia = acontecimiento;
+    implicados = implicadosnumero;
+}
 anecdotario::~anecdotario()
 {
     //dtor
