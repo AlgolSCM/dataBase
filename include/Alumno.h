8bd5a2e42d000a248630a3b26ae43eb8eb3f0187
magrcruz <50184830+magrcruz@users.noreply.github.com>
2019-11-01 00:07:11 -0500

    Eliminacion de algunas funciones,

--- include/Alumno.h
+++ include/Alumno.h
@@ -17,33 +17,34 @@ class Alumno
     public:
         Alumno();//Matriccula
         virtual ~Alumno();
-        Alumno(string apellidos,string nombres);
+        Alumno(string nombres,string apellidos);
 
-        void menuSeleccionAlumno();
-        void visualizarInformacion();//visualiza la informacion y pregunta si desea modificar algo
-        void seleccionarDatoModificar();
+        void texto(string label,string texto[],int tam,bool prompt=1, bool numeracion=1);
+        void menuSeleccionAlumno(bool inAlumno=1);
+        void visualizarDatos();//visualiza la informacion y pregunta si desea modificar algo
+        //void seleccionarDatoModificar();
 
-        void setNombresApellidos(string apellidos, string nombres,char genero, fecha nacimiento);
-        void setDocumentos(int dni,int partidaNacimiento);
-        void setTelefonoDireccion(int telefono, direccion domicilio);
+        //SETS
+        void setNombresApellidos(string apellidos, string nombres,char genero='F');
+        void setNacimiento(int,int,int);
+        void setDocumentos(int dni,int partidaNacimiento=0);
+        void setTelefonos(int,int);//la direccion desde la clase
         void setcodigoEstudiante(int codigo);
         void setReligion(string religion);
         void setPagoDeuda(int n);//restar el saldo pendiente en n unidades
 
-        void getSeccion();
+        //GETS
+        string getSeccion();
         string getNombres();
         string getApellidos();
-        void getDNI();
-        void getPartida();
-        void getTelefono();
-        void getDireccion();
-        void getcodigoEstudiante();
-        void getReligion();
-        void getNotas();
+        string getApellidosNombres();
+        long long getcodigoEstudiante();
 
     private:
-        string nombre, apellido;
-        int dni, partidaNacimiento, telefonoEmergencia[2];
+        string nombres, apellidos;
+        string seccion;
+        int dni, partidaNacimiento;
+        int telefonoEmergencia[2];
         long long codigoEstudiante;//codigo otorgado por el siagie
         char genero;//M o F
         fecha nacimiento;
@@ -51,7 +52,7 @@ class Alumno
         string religion;
         cursos listaCursos[10];//cursos y talleres
         Padres nombrePadres[2];//padre/madre/apoderado
-        int codigoMatricula,saldoPendiente;
+        int codigoMatricula,deuda;
         vector <char> asistencias;
 };
 
