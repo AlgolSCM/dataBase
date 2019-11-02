7b7ae6ac0b3b9d3eeb8fc1aab41640a52a54f665
camilaalj <54364115+camilaalj@users.noreply.github.com>
2019-11-01 20:12:43 -0500

    Update anecdotario.cpp

--- src/anecdotario.cpp
+++ src/anecdotario.cpp
@@ -20,3 +20,21 @@ anecdotario::~anecdotario()
 {
     //dtor
 }
+
+void anecdotario::suceso(string titulo){
+    this->titulo=titulo;
+}
+void anecdotario::descripcion(string detalles){
+    this->detalles=detalles;
+}
+void anecdotario::acontecimiento(fecha acontecimiento){
+    this->fecha acontecimiento= fecha acontecimiento;
+}
+void anecdotario::implicados(int numero, string alumnos){
+    this->numero=numero;
+    this->alumnos=alumnos;
+}
+void anecdotario::MostrarAnecdota()
+    cout<<endl<<"=-=-=-=-=-=-=-=-=-ANECDOTARIO=-=-=-=-=-=-=-=-=-=-=-=-"<<endl;
+
+
