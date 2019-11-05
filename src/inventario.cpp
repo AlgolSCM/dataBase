9b60c7bcc357971b8cbabc8d96e4039e4502271e
camilaalj <54364115+camilaalj@users.noreply.github.com>
2019-11-05 02:27:48 -0500

    Update inventario.cpp

--- src/inventario.cpp
+++ src/inventario.cpp
@@ -1,11 +1,6 @@
 
 #include "inventario.h"
 
-inventario::inventario()
-{
-    //ctor
-}
-
 inventario::inventario(){
     numeromesas=0; numerosillas=0; numeroestantes=0;
     mesasbuenestado=0;sillasbuenestado=0;estantesbuenestado=0;
@@ -18,27 +13,31 @@ inventario::~inventario()
     //dtor
 }
 
-void inventario:: mesas (int numesas, int buenestadosi, int malestadosi){
-     this->numeromesas=numesas;
+void inventario:: mesas (int numeromesas, int buenestadome, int mesasmalestado){
+     this->numeromesas=numeromesas;
      cout<< "Ingrese el numero de sillas en buen estado:"<<endl;
-     cin>> buenestadosi;
+     cin>> buenestadome;
      cout<< "Ingrese el numero de sillas en mal estado: "<<endl;
-     cin>> malestadosi;
-     while (buenestadosi+malestadosi==numesas){
-        if ((buenestadosi==numesas)&& (malestadosi==0)){
+     this->mesasmalestado=mesasmalestado;
+     cin>> mesasmalestado;
+     while (buenestadome+mesasmalestado==numeromesas){
+        if ((buenestadome==numeromesas)&& (mesasmalestado==0)){
             cout<<"Gracias, el inventario de sillas fue completado con exito"<<endl;
         }
-        if (malestadosi>0){
-            this->mesasmalestado=malestadosi;
+        if (mesasmalestado>0){
             cout<<"Ingrese el nombre del articulo dañado: "<<endl;
-            cin>> string articulo;
+            string articulo;
+            cin>> articulo;
             this->nombre=articulo;
             cout<<"Ingrese el color del articulo dañado: "<<endl;
-            cin>> string articulocolor;
+            string articulocolor;
+            cin>> articulocolor;
             this->color=articulocolor;
             cout<<"Ingrese el material del articulo dañado: "<<endl;
-            cin>>string material;
+            string material;
+            cin>> material;
             this->modelo=material;
         }
      }
 }
+
