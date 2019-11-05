819714800730f698a95f88c5104f19a4d14bd250
camilaalj <54364115+camilaalj@users.noreply.github.com>
2019-11-05 02:01:43 -0500

    Update inventario.cpp

--- src/inventario.cpp
+++ src/inventario.cpp
@@ -1,3 +1,4 @@
+
 #include "inventario.h"
 
 inventario::inventario()
@@ -17,3 +18,27 @@ inventario::~inventario()
     //dtor
 }
 
+void inventario:: mesas (int numesas, int buenestadosi, int malestadosi){
+     this->numeromesas=numesas;
+     cout<< "Ingrese el numero de sillas en buen estado:"<<endl;
+     cin>> buenestadosi;
+     cout<< "Ingrese el numero de sillas en mal estado: "<<endl;
+     cin>> malestadosi;
+     while (buenestadosi+malestadosi==numesas){
+        if ((buenestadosi==numesas)&& (malestadosi==0)){
+            cout<<"Gracias, el inventario de sillas fue completado con exito"<<endl;
+        }
+        if (malestadosi>0){
+            this->mesasmalestado=malestadosi;
+            cout<<"Ingrese el nombre del articulo dañado: "<<endl;
+            cin>> string articulo;
+            this->nombre=articulo;
+            cout<<"Ingrese el color del articulo dañado: "<<endl;
+            cin>> string articulocolor;
+            this->color=articulocolor;
+            cout<<"Ingrese el material del articulo dañado: "<<endl;
+            cin>>string material;
+            this->modelo=material;
+        }
+     }
+}
