80ffbac9083e8d3faab02ad5cdb02f4f72d54784
camilaalj <54364115+camilaalj@users.noreply.github.com>
2019-11-20 09:55:41 -0500

    Cambios

--- include/anecdotario.h
+++ include/anecdotario.h
@@ -11,22 +11,13 @@ class anecdotario
     public:
         anecdotario();
         virtual ~anecdotario();
-        anecdotario(int implicadosnumero,string suceso, string descripcionde, fecha acontecimiento);
-        void suceso(string titulo);
-        void describir(string detalles);
-        void acontecimiento(fecha acontecimiento);
-        void involucrados(int numero, string alumnos);
-        void MostrarAnecdota();
+        anecdotario(int implicados,string titulo, string descripcion, string alumnos, fecha dia);
+        //void MostrarAnecdota();
     protected:
-
     private:
-//        vector <>;
         string descripcion, titulo,alumnos;
         int implicados;
-        //fecha dia;
-
-
-
+        fecha dia;
 };
 
 #endif // ANECDOTARIO_H
