44d3c9c334817c0be7bc2e65ab8b167fd9ac28a0
Solcito25 <51237026+Solcito25@users.noreply.github.com>
2019-11-22 09:41:59 -0500

    abc

--- include/NotasABC.h
+++ include/NotasABC.h
@@ -1,12 +1,12 @@
 #ifndef NOTASABC_H
 #define NOTASABC_H
 #include<notas.h>
-
+#include <stdexcept>
 
 class NotasABC: public notas
 {
     public:
-        NotasABC();
+        //NotasABC();
         NotasABC(int trimestre_,string nombreItem_, string comentario_,char nota_);
         bool validar(char nota);
         virtual ~NotasABC();
