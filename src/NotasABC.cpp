5579f5069205b5b37473748001952172e7099eda
Solcito25 <51237026+Solcito25@users.noreply.github.com>
2019-11-22 09:43:19 -0500

    abc

--- src/NotasABC.cpp
+++ src/NotasABC.cpp
@@ -1,11 +1,22 @@
 #include "NotasABC.h"
-
-NotasABC::NotasABC()
+#include<string>
+#include <stdexcept>
+#include "notas.h"
+using namespace std;
+NotasABC::NotasABC(int trimestre_,string nombreItem_, string comentario_,char nota_)
+  : notas(trimestre_, nombreItem_, comentario_)
 {
-    //ctor
+    nota=nota_;
 }
 
 NotasABC::~NotasABC()
 {
     //dtor
+}
+bool NotasABC ::validar(char nota){
+ if (nota== 'A'||nota== 'B'|| nota== 'C'){return true;}
+
+ else
+    throw invalid_argument("Usted no puede ingresar ese caracter");
+
 }
